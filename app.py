542f003288d7d91f8696bd944eea79b25e0641a2
Haidar Ramdhani <159707550+HaidarRamdhani@users.noreply.github.com>
2025-05-22 10:09:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 
 st.set_page_config(page_title="Dashboard Analisis IndoBERT dan BERTopic", layout="wide")
-st.title("📊 Dashboard Analisis Sentimen dan Topik Komentar YouTube 2")
+st.title("📊 Dashboard Analisis Sentimen dan Topik Komentar YouTube")
 
 # --------------------------
 # Upload dan Baca File
