a4e0732ea8ed1b4f26b48143283b1d6e00c5318a
Haidar Ramdhani <159707550+HaidarRamdhani@users.noreply.github.com>
2025-06-03 09:39:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
+import plotly.express as px
 import emoji # Untuk demojize emoji di preprocessing
 from googleapiclient.discovery import build # Untuk YouTube API
 from transformers import pipeline # Untuk model sentiment fine-tuned Anda
